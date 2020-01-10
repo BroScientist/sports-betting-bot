275315dd23b1abb49a7d0583369eff090418f758
Victor <apple@MacBook-Pro.local>
2020-01-09 22:39:56 -0800

    add all essentially functions

--- get_predicted_scores.py
+++ get_predicted_scores.py
@@ -1,13 +1,32 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
 import csv
+import datetime
+
+links = []
+
+driver = webdriver.Chrome('/Users/apple/PycharmProjects/chromedriver')
+driver.get('https://www.whoscored.com/Previews')
+page = driver.page_source
+soup = BeautifulSoup(page, 'html.parser')
+previews_date = soup.find('td', {'class': 'previews-date'}).text.strip()
+import datetime
+tmr_date = str(datetime.date.today() + datetime.timedelta(days=1))
+
+previews = soup.find('div', {'class': 'region previews'})
+# previews = previews.find_all('tr', {'class': 'alt'})
+match_links = previews.find_all('a', href=True)
+# i is the number of links we want to reach down -1 
+for i in range(9):
+    if 'Tournaments' not in match_links[i]['href']:
+        links.append('https://www.whoscored.com/' + match_links[i]['href'])
 
 
-def get_predictions_for_match(match_url):
+def get_predictions_for_match(driver, match_url):
     # match_url is whoscored.com preview link
     # this works but it's slow as shit (20+ seconds for one page...) but I don't wanna get banned again
     # it's slow because we are downloading the entire page... fuck this
-    driver = webdriver.Chrome('/Users/apple/PycharmProjects/chromedriver')
+    # driver = webdriver.Chrome('/Users/apple/PycharmProjects/chromedriver')
     driver.get(match_url)
     page = driver.page_source
 
@@ -29,9 +48,10 @@ def get_predictions_for_match(match_url):
 
 
 # TODO write code to only find links for tmr's matches (or an arbitray range of days)
-links = []
-# for link in links:
-#     get_predictions_for_match(link)
+with open('predictions.csv', 'w') as f:
+    pass
 
-if __name__ == "__main__":
-    get_predictions_for_match('https://www.whoscored.com/Matches/1376096/Preview/England-Premier-League-2019-2020-Crystal-Palace-Arsenal')
+for link in links:
+    get_predictions_for_match(driver, link)
+
+driver.quit()
--- place_bets.py
+++ place_bets.py
@@ -5,6 +5,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 import params
+import csv
 
 driver = webdriver.Chrome('/Users/apple/PycharmProjects/chromedriver')
 url = 'https://sports.partypoker.com/en/sports'
@@ -24,51 +25,67 @@ password_field.send_keys(Keys.ENTER)
 # locate the sports search button
 
 # wait = WebDriverWait(driver, 15)
-# search_bar = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "i[class='ui-icon ui-icon-size-lg sports-icon theme-search before-separator ng-star-inserted']")))
+# sports_search = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "i[class='ui-icon ui-icon-size-lg sports-icon theme-search before-separator ng-star-inserted']")))
+# sports_search.click()
 
-time.sleep(10)
-driver.find_element_by_css_selector("i[class='ui-icon ui-icon-size-lg sports-icon theme-search before-separator ng-star-inserted']").click()
 
-# locate the search bar and send keys
-search_bar = driver.find_element_by_css_selector("input[name='searchField']")
-# search box can accept inputs for both teams ex. aston villa leicester city keys.Enter
-search_bar.send_keys('Sheffield United West Ham')
-search_bar.send_keys(Keys.ENTER)
-time.sleep(2)
-bet_link = driver.find_element_by_css_selector("ms-event[class='grid-event ms-active-highlight ng-star-inserted']")
-bet_link.click()
-time.sleep(5)
+global initial_run_completed
+initial_run_completed = False
 
-# locate the Correct Score betting page (it's simpler than scrolling down the page)
-# TODO write code to accept input from file
-correct_score_box = driver.find_element_by_xpath("//*[text()='Correct score']")
-correct_score_box.click()
-correct_score = '2-1'
-score_box = driver.find_element_by_xpath(f".//div[text() = '{correct_score}']")
-score_box.click()
 
-# maximize the window because the bet box won't show up otherwise
-driver.maximize_window()
+def place_bet(driver, team_names, correct_score):
+    time.sleep(5)
+    driver.find_element_by_css_selector("i[class='ui-icon ui-icon-size-lg sports-icon theme-search before-separator ng-star-inserted']").click()
 
-# locate the input field, delete the default value of 5.00 and input the preset stake $$$ from params.py
-stake_input = driver.find_element_by_css_selector("input[tabindex='-1']")
-for _ in range(4):
-    stake_input.send_keys(Keys.BACKSPACE)
+    # locate the search bar and send keys
+    search_bar = driver.find_element_by_css_selector("input[name='searchField']")
+    # search box can accept inputs for both teams ex. aston villa leicester city keys.Enter
+    search_bar.send_keys(team_names)
+    search_bar.send_keys(Keys.ENTER)
+    time.sleep(5)
+    try:
+        global initial_run_completed
+        if not initial_run_completed:
+            bet_link = driver.find_element_by_css_selector("span[class='bet-builder-icon ng-star-inserted']")
+            initial_run_completed = True
+        else:
+            bet_link = driver.find_element_by_css_selector("div[class='participant-container ng-star-inserted']")
+        try:
+            bet_link.click()
+        except:
+            driver.execute_script("arguments[0].click();", bet_link)
+        time.sleep(8)
+        # locate the Correct Score betting page (it's simpler than scrolling down the page)
+        # TODO write code to accept input from file
+        correct_score_box = driver.find_element_by_xpath("//*[text()='Correct score']")
+        correct_score_box.click()
+        score_box = driver.find_element_by_xpath(f".//div[text() = '{correct_score}']")
+        score_box.click()
 
-stake_input.send_keys(params.STAKE)
+        # maximize the window because the bet box won't show up otherwise
+        driver.maximize_window()
 
-# click the Place Bet button
-submit_btn = driver.find_element_by_css_selector("button[class='place betslip-place-button']")
-submit_btn.click()
+        # locate the input field, delete the default value of 5.00 and input the preset stake $$$ from params.py
+        stake_input = driver.find_element_by_css_selector("input[tabindex='-1']")
+        # stake_input.clear()
+        for _ in range(4):
+            stake_input.send_keys(Keys.BACKSPACE)
 
-# TODO write code to return to start and place new bet
-'''
-we can just click the search bar again and insert new input from csv
+        stake_input.send_keys(params.STAKE)
 
-login and stuff
-click search bar
-for row in csv:
-    place_bets(driver=driver, teams=row[0], correct_score=row[1])
-    click search bar
-finish
-'''
+        # click the Place Bet button
+        submit_btn = driver.find_element_by_css_selector("button[class='place betslip-place-button']")
+        submit_btn.click()
+    except:
+        print('no results found')
+    finally:
+        print('bet successfully placed')
+
+
+with open('predictions.csv', 'r') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        team_names = row[0]
+        predicted_score  = row[1]
+        place_bet(driver, team_names, predicted_score)
+    driver.quit()
