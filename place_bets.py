043d7972acdb96a13f3fffeebc0ac2536b0881ca
Victor <apple@Victors-MBP.hitronhub.home>
2020-01-11 18:08:48 -0800

    add exception handling with empty search results

--- place_bets.py
+++ place_bets.py
@@ -12,8 +12,12 @@ url = 'https://sports.partypoker.com/en/sports'
 driver.get(url)
 # page = driver.page_source
 
+
 # login to Partypoker.com
-time.sleep(3)
+WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located)
+# login_btn = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "vn-menu-item[linkclass='header-btn btn']")))
+# login_btn.click()
+# time.sleep(0.5)
 driver.find_element_by_css_selector("vn-menu-item[linkclass='header-btn btn']").click()
 user_name_field = driver.find_element_by_css_selector("input[name='username']")
 user_name_field.click()
@@ -22,21 +26,20 @@ password_field = driver.find_element_by_css_selector("input[name='password']")
 password_field.send_keys(params.PASSWORD)
 password_field.send_keys(Keys.ENTER)
 
-# locate the sports search button
-
-# wait = WebDriverWait(driver, 15)
-# sports_search = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "i[class='ui-icon ui-icon-size-lg sports-icon theme-search before-separator ng-star-inserted']")))
-# sports_search.click()
 
 
 global initial_run_completed
 initial_run_completed = False
 
 
-def place_bet(driver, team_names, correct_score):
-    time.sleep(5)
-    driver.find_element_by_css_selector("i[class='ui-icon ui-icon-size-lg sports-icon theme-search before-separator ng-star-inserted']").click()
-
+def place_bet(driver, team_names, correct_score, stake):
+    # locate the sports search button
+    WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located)
+    try:
+        driver.find_element_by_css_selector("i[class='ui-icon ui-icon-size-lg sports-icon theme-search before-separator ng-star-inserted']").click()
+    except:
+        time.sleep(8)
+        driver.find_element_by_css_selector("i[class='ui-icon ui-icon-size-lg sports-icon theme-search before-separator ng-star-inserted']").click()
     # locate the search bar and send keys
     search_bar = driver.find_element_by_css_selector("input[name='searchField']")
     # search box can accept inputs for both teams ex. aston villa leicester city keys.Enter
@@ -54,14 +57,13 @@ def place_bet(driver, team_names, correct_score):
             bet_link.click()
         except:
             driver.execute_script("arguments[0].click();", bet_link)
-        time.sleep(7)
-        # locate the Correct Score betting page (it's simpler than scrolling down the page)
-        # TODO write code to accept input from file
+        # WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located) this doesn't work for some reason
+        time.sleep(5)
+        # locate the Correct Score betting page (it's simpler than scrolling down the page to find the Correct Score section)
         correct_score_box = driver.find_element_by_xpath("//*[text()='Correct score']")
         correct_score_box.click()
         score_box = driver.find_element_by_xpath(f".//div[text() = '{correct_score}']")
         score_box.click()
-
         # maximize the window because the bet box won't show up otherwise
         driver.maximize_window()
 
@@ -71,24 +73,26 @@ def place_bet(driver, team_names, correct_score):
         for _ in range(4):
             stake_input.send_keys(Keys.BACKSPACE)
 
-        stake_input.send_keys(params.STAKE)
+        stake_input.send_keys(stake)
 
         # click the Place Bet button
         submit_btn = driver.find_element_by_css_selector("button[class='place betslip-place-button']")
         submit_btn.click()
+        print(f'Bet: {team_names} {correct_score} (${stake}) has been placed')
     except Exception:
-        print(Exception)
-        print('no results found')
+        print(f'Problem finding bets for: {team_names}')
         driver.get('https://sports.partypoker.com/en/sports')
-        time.sleep(4)
-    finally:
-        print('bet successfully processed')
+        time.sleep(5)
+        raise Exception
 
 
 with open('predictions.csv', 'r') as f:
     reader = csv.reader(f)
     for row in reader:
         team_names = row[0]
-        predicted_score  = row[1]
-        place_bet(driver, team_names, predicted_score)
+        predicted_score = row[1]
+        try:
+            place_bet(driver, team_names, predicted_score, params.STAKE)
+        except Exception:
+            continue
     driver.quit()
